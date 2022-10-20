b586e13c936db974ab5820bf1e82b8d263151de7
Jarek Zgoda <jarek.zgoda@gmail.com>
2022-10-20 13:40:45 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,5 @@
+# My Heart Health
+
+Collect blood pressure readings
+
+Another attempt at mobile dev with Flutter.
---
+++ android/app/src/main/kotlin/org/zgodowie/mhh/MainActivity.kt
@@ -0,0 +1,6 @@
+package org.zgodowie.mhh
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
