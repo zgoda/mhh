dc0d3f05c42e2be7273585a179ef94d38cd2c5e8
Jarek Zgoda <jarek.zgoda@gmail.com>
2022-10-26 18:51:19 +0200

    Removed unused package from deps

--- lib/pages/newrecording.dart
+++ lib/pages/newrecording.dart
@@ -1,17 +1,16 @@
 import 'package:date_time_picker/date_time_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mhh/models.dart';
 
 const defaultTextfieldInsets = EdgeInsets.fromLTRB(20, 15, 20, 15);
 const defaultFieldSpacer = SizedBox(height: 25);
 
-class RecordingCreateScreen extends HookConsumerWidget {
+class RecordingCreateScreen extends HookWidget {
   const RecordingCreateScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     final now = DateTime.now();
     final recordDateController = useTextEditingController(text: now.toString());
     final recordSysPressureController = useTextEditingController();
